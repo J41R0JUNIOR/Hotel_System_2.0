b4f4c0d27e33e6779e97c55e2bf867898b333c2a
Jairo Júnior <81876783+J41R0JUNIOR@users.noreply.github.com>
2025-03-10 10:45:30 -0300

    .

--- Hotel_Project/src/Group.java
+++ Hotel_Project/src/Group.java
@@ -37,7 +37,7 @@ public class Group extends Thread {
             else {
                 runLoop = false;
 //                this.interrupt();
-        
+                System.out.println("finalizando..." + this.id);
             }
         }
     }
